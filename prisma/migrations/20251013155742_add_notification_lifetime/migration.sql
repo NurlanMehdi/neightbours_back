89e4ccb068c209bb143f0415362628f0ad3a79f6
nurlanguleliyev <nurlanguleliyev@gmail.com>
2025-10-14 12:12:35 +0200

    disable sockets for chats

--- prisma/migrations/20251013155742_add_notification_lifetime/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."notifications" ADD COLUMN     "expiresAt" TIMESTAMP(3),
-ADD COLUMN     "lifetimeHours" INTEGER NOT NULL DEFAULT 24;
-
--- CreateIndex
-CREATE INDEX "notifications_expiresAt_idx" ON "public"."notifications"("expiresAt");
